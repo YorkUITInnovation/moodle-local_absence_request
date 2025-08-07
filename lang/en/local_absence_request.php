833dd9874284bd6b5f6e801e5cd40007bc25aebd
Patrick Thibaudeau <thibaud@yorku.ca>
2025-08-07 14:31:09 -0400

    Updated language strings

--- lang/en/local_absence_request.php
+++ lang/en/local_absence_request.php
@@ -1,40 +1,40 @@
 <?php
 // Language strings for absence_request plugin.
 $string['absence_end'] = 'Absence End Date and Time';
-$string['absence_request'] = 'Absence Request';
+$string['absence_request'] = 'Report an Absence';
 $string['absence_start'] = 'Absence Start Date and Time';
 $string['all_faculties'] = 'All Faculties';
 $string['back_to_my_courses'] = 'Back to My Courses';
 $string['bereavement'] = 'Bereavement of an immediate family member';
 $string['course'] = 'Course';
 $string['duration'] = 'Duration (days)';
-$string['error_academic_year'] = 'The absence request must be within the current academic year.';
-$string['error_already_submitted_for_selected_dates'] = 'You have already submitted an absence request for the selected dates.';
+$string['error_academic_year'] = 'The reported absence must be within the current academic year.';
+$string['error_already_submitted_for_selected_dates'] = 'You have already submitted an absence for the selected dates.';
 $string['error_end_before_start'] = 'The end date cannot be before the start date.';
 $string['error_max_7_days'] = 'The absence cannot be longer than 7 days.';
-$string['error_term_period'] = 'The absence request must be within the current term period.';
+$string['error_term_period'] = 'The absence must be within the current term period.';
 $string['faculty'] = 'Faculty';
 $string['from_date'] = 'From Date';
-$string['max_requests_reached'] = 'You have reached the maximum number of absence requests for this term.';
+$string['max_requests_reached'] = 'You have reached the maximum number of absences for this term.';
 $string['nopermission'] = 'You do not have permission to view this page.';
-$string['not_eligible'] = 'You are not eligible to submit an absence request.';
-$string['not_enrolled_in_courses'] = 'You are not enrolled in any courses for this term. You can only submit absence requests for courses you are enrolled in.';
-$string['notify_instructor_body'] = 'A student has submitted an absence request. View the report: {$a}';
-$string['notify_instructor_subject'] = 'Student Absence Request Notification';
-$string['pluginname'] = 'Absence Request';
-$string['report_link'] = 'View Absence Requests Report';
-$string['request_submitted'] = 'Your absence request has been submitted. Note: It is the responsibility of the student to arrange any accommodations for missed course work with your individual instructors';
+$string['not_eligible'] = 'You are not eligible to report an absence.';
+$string['not_enrolled_in_courses'] = 'You are not enrolled in any courses for this term. You can only report absences for courses you are enrolled in.';
+$string['notify_instructor_body'] = 'A student has reported an absence. View the report: {$a}';
+$string['notify_instructor_subject'] = 'Student Reported Absence Notification';
+$string['pluginname'] = 'Absence Reporting';
+$string['report_link'] = 'View Reported Absences Report';
+$string['request_submitted'] = 'Your reported absence has been submitted. Note: It is the responsibility of the student to arrange any accommodations for missed course work with your individual instructors';
 $string['short_term_health'] = 'Short-term health conditions (illness, physical injury, scheduled surgery)';
 $string['sisid'] = 'Student ID';
 $string['student_firstname'] = 'Student First Name';
-$string['student_instructions'] = '<p>Please fill out the form below to submit your absence request. You can only submit up to two requests per term. '
-    . 'Ensure that your request does not exceed 7 days in duration. Any reason beyond those listed in the drop-down '
-    . 'menu are not eligible for self-reported absence. Note that this request will be submitted for all courses you are enrolled in, in this term.</p>'
+$string['student_instructions'] = '<p>Please fill out the form below to report an absence. You can only submit up to two absences per term. '
+    . 'Ensure that your absence does not exceed 7 days in duration. Any reason beyond those listed in the drop-down '
+    . 'menu are not eligible for a self-reported absence. Note that this reported absence will be submitted for all courses you are enrolled in, in this term.</p>'
     . '<p>Any accommodations for missed course work need to be arranged with your individual instructors</p>'
     . '<p>For more information about the policy on Academic Consideration for Missed Work, please refer to the '
     . '<a href="20250227_Senate_approved_Acad Consid_for_Missed_Course_Work_Policy_Final.pdf" target="_blank">Academic Consideration Policy</a>.</p>';
 $string['student_lastname'] = 'Student Last Name';
-$string['submit_request'] = 'Submit Absence Request';
+$string['submit_request'] = 'report An Absence';
 $string['to_date'] = 'To Date';
 $string['type_of_circumstance'] = 'Type of Circumstance';
 $string['unforeseen'] = 'Unforeseen or unavoidable incidents beyond the student’s control';
@@ -42,14 +42,14 @@ $string['view_faculty_report'] = 'Faculty Absence Report';
 
 // Settings page strings
 $string['requests_per_term'] = 'Number of requests per term';
-$string['requests_per_term_desc'] = 'Set the maximum number of absence requests a student can submit per term.';
+$string['requests_per_term_desc'] = 'Set the maximum number of reported absences a student can submit per term.';
 $string['teacher_message'] = 'Teacher message';
-$string['teacher_message_desc'] = 'Default message to be sent to teachers when an absence request is submitted.';
+$string['teacher_message_desc'] = 'Default message to be sent to teachers when an absence is reported.';
 $string['student_message'] = 'Student message';
-$string['student_message_desc'] = 'Default message to be sent to students when an absence request is submitted.';
+$string['student_message_desc'] = 'Default message to be sent to students when an absence is reported.';
 $string['use_period'] = 'Use Period';
 $string['use_period_desc'] = 'This setting is only to be used while testing the plugin. '
-    . 'It allows you to select the period during which absence requests can be submitted. '
+    . 'It allows you to select the period during which reported absences can be submitted. '
     . 'Note: Year is always available as Winter, never fall. So if testing for Year courses, '
     . 'make sure you select a date between January and April.';
 $string['use_period_no'] = 'No';
@@ -62,10 +62,10 @@ $string['use_period_Y'] = 'Year';
 $string['absence_request:view_faculty_report'] = 'View Faculty Absence Report';
 $string['absence_request:view_teacher_report'] = 'View Absence Report';
 $string['messageprovider:absence_notification'] = 'Absence Request Notifications';
-$string['student_message_subject'] = 'Absence Request Submitted';
-$string['teacher_message_subject'] = 'Absence Request Notification';
+$string['student_message_subject'] = 'Absence Reported Successfully';
+$string['teacher_message_subject'] = 'Reported Absence Notification';
 $string['student_message'] = 'Hello {$a->firstname}, '
-    .'<p>Your absence request has been successfully submitted.</p>'
+    .'<p>Your reported absence has been successfully submitted.</p>'
     . '<p>'
     . '<b>Circumstance:</b> {$a->circumstance}<br>'
     . '<b>Start date:</b> {$a->startdate}<br>'
@@ -85,22 +85,22 @@ $string['teacher_message'] = 'Hello, <p>You have received a self-reported absenc
     . '<p>Thank you!</p>';
 
 // Privacy API strings
-$string['privacy:metadata:local_absence_request'] = 'Information about student absence requests';
-$string['privacy:metadata:local_absence_request:userid'] = 'The ID of the user who submitted the absence request';
-$string['privacy:metadata:local_absence_request:faculty'] = 'The faculty associated with the absence request';
-$string['privacy:metadata:local_absence_request:circumstance'] = 'The circumstance for the absence request';
+$string['privacy:metadata:local_absence_request'] = 'Information about student reported absence';
+$string['privacy:metadata:local_absence_request:userid'] = 'The ID of the user who reported the absence';
+$string['privacy:metadata:local_absence_request:faculty'] = 'The faculty associated with the reported absence';
+$string['privacy:metadata:local_absence_request:circumstance'] = 'The circumstance for the reported absence';
 $string['privacy:metadata:local_absence_request:starttime'] = 'The start time of the absence';
 $string['privacy:metadata:local_absence_request:endtime'] = 'The end time of the absence';
-$string['privacy:metadata:local_absence_request:acadyear'] = 'The academic year of the absence request';
-$string['privacy:metadata:local_absence_request:termperiod'] = 'The term period of the absence request';
-$string['privacy:metadata:local_absence_request:timecreated'] = 'The time the absence request was created';
+$string['privacy:metadata:local_absence_request:acadyear'] = 'The academic year of the reported absence';
+$string['privacy:metadata:local_absence_request:termperiod'] = 'The term period of the reported absence';
+$string['privacy:metadata:local_absence_request:timecreated'] = 'The time the reported absence was created';
 
-$string['privacy:metadata:local_absence_req_teacher'] = 'Information about teachers associated with absence requests';
-$string['privacy:metadata:local_absence_req_teacher:userid'] = 'The ID of the teacher associated with the absence request';
-$string['privacy:metadata:local_absence_req_teacher:absence_req_course_id'] = 'The ID of the absence request course';
+$string['privacy:metadata:local_absence_req_teacher'] = 'Information about teachers associated with reported absence';
+$string['privacy:metadata:local_absence_req_teacher:userid'] = 'The ID of the teacher associated with the reported absence';
+$string['privacy:metadata:local_absence_req_teacher:absence_req_course_id'] = 'The ID of the reported absence course';
 $string['privacy:metadata:local_absence_req_teacher:timecreated'] = 'The time the teacher association was created';
 
-$string['privacy:path:absencerequests'] = 'Absence requests';
+$string['privacy:path:absencerequests'] = 'Reported absences';
 $string['privacy:path:teacherrequests'] = 'Teacher notifications';
 
 $string['teacher_firstname'] = 'Teacher First Name';
--- lang/fr/local_absence_request.php
+++ lang/fr/local_absence_request.php
@@ -1,57 +1,51 @@
 <?php
-// Fichier de langue pour le plugin absence_request (français)
-$string['absence_end'] = "Date et heure de fin d'absence";
-$string['absence_request'] = "Demande d'absence";
-$string['absence_start'] = "Date et heure de début d'absence";
-$string['all_faculties'] = "Toutes les facultés";
-$string['back_to_my_courses'] = "Retour à mes cours";
-$string['bereavement'] = "Décès d'un membre de la famille immédiate";
-$string['course'] = "Cours";
-$string['duration'] = "Durée (jours)";
-$string['error_academic_year'] = "La demande d'absence doit être dans l'année académique en cours.";
-$string['error_already_submitted_for_selected_dates'] = "Vous avez déjà soumis une demande d'absence pour les dates sélectionnées.";
-$string['error_end_before_start'] = "La date de fin ne peut pas être antérieure à la date de début.";
-$string['error_max_7_days'] = "L'absence ne peut pas dépasser 7 jours.";
-$string['error_term_period'] = "La demande d'absence doit être dans la période de session en cours.";
-$string['faculty'] = "Faculté";
-$string['from_date'] = "Date de début";
-$string['max_requests_reached'] = "Vous avez atteint le nombre maximum de demandes d'absence pour cette session.";
-$string['nopermission'] = "Vous n'avez pas la permission de voir cette page.";
-$string['not_eligible'] = "Vous n'êtes pas éligible pour soumettre une demande d'absence.";
-$string['not_enrolled_in_courses'] = "Vous n'êtes inscrit à aucun cours pour cette session. Vous ne pouvez soumettre des demandes d'absence que pour les cours auxquels vous êtes inscrit.";
-$string['notify_instructor_body'] = "Un étudiant a soumis une demande d'absence. Voir le rapport : {$a}";
-$string['notify_instructor_subject'] = "Notification de demande d'absence d'un étudiant";
-$string['pluginname'] = "Demande d'absence";
-$string['report_link'] = "Voir le rapport des demandes d'absence";
-$string['request_submitted'] = "Votre demande d'absence a bien été soumise. Remarque: Il est de la responsabilité de l'étudiant de prendre les dispositions nécessaires avec ses enseignants pour s'adapter aux cours manqués.";
-$string['short_term_health'] = "Problèmes de santé à court terme (maladie, blessure physique, chirurgie programmée)";
-$string['sisid'] = 'ID étudiant';
-$string['student_firstname'] = 'Prénom de l\'étudiant';
-$string['student_instructions'] = '<p>Veuillez remplir le formulaire ci-dessous pour soumettre votre demande d\'absence. Vous ne pouvez soumettre que deux demandes par session. '
-    . 'Assurez-vous que votre demande ne dépasse pas 7 jours. Toute raison autre que celles énumérées dans le menu déroulant '
-    . 'n’est pas admissible à une absence autodéclarée.Cette demande sera soumise pour tous les cours auxquels vous êtes inscrit cette session.</p>'
-    . '<p>Toute adaptation pour le travail de cours manqué doit être organisée avec vos instructeurs individuels</p>'
-    . '<p>Pour plus d\'informations sur la politique de considération académique pour le travail de cours manqué, veuillez consulter la '
-    . '<a href="20250227_Senate_approved_Acad Consid_for_Missed_Course_Work_Policy_Final.pdf" target="_blank">politique de considération académique</a>.</p>';
-$string['student_lastname'] = 'Nom de famille de l\'étudiant';
-$string['submit_request'] = "Soumettre une demande d'absence";
-$string['to_date'] = "Date de fin";
-$string['type_of_circumstance'] = "Type de circonstance";
-$string['unforeseen'] = "Incidents imprévus ou inévitables hors du contrôle de l'étudiant";
-$string['view_faculty_report'] = 'Rapport d\'absence par faculté';
+// Language strings for absence_request plugin (French).
+$string['absence_end'] = 'Date et heure de fin de l’absence';
+$string['absence_request'] = 'Signaler une absence';
+$string['absence_start'] = 'Date et heure de début de l’absence';
+$string['all_faculties'] = 'Toutes les facultés';
+$string['back_to_my_courses'] = 'Retour à mes cours';
+$string['bereavement'] = 'Deuil d’un membre de la famille proche';
+$string['course'] = 'Cours';
+$string['duration'] = 'Durée (jours)';
+$string['error_academic_year'] = 'L’absence signalée doit se situer dans l’année universitaire en cours.';
+$string['error_already_submitted_for_selected_dates'] = 'Vous avez déjà signalé une absence pour les dates sélectionnées.';
+$string['error_end_before_start'] = 'La date de fin ne peut pas être antérieure à la date de début.';
+$string['error_max_7_days'] = 'L’absence ne peut pas dépasser 7 jours.';
+$string['error_term_period'] = 'L’absence doit se situer dans la période du trimestre en cours.';
+$string['faculty'] = 'Faculté';
+$string['from_date'] = 'Date de début';
+$string['max_requests_reached'] = 'Vous avez atteint le nombre maximum d’absences pour ce trimestre.';
+$string['nopermission'] = 'Vous n’avez pas la permission de voir cette page.';
+$string['not_eligible'] = 'Vous n’êtes pas éligible pour signaler une absence.';
+$string['not_enrolled_in_courses'] = 'Vous n’êtes inscrit à aucun cours pour ce trimestre. Vous pouvez uniquement signaler des absences pour les cours auxquels vous êtes inscrit.';
+$string['notify_instructor_body'] = 'Un étudiant a signalé une absence. Voir le rapport : {$a}';
+$string['notify_instructor_subject'] = 'Notification d’absence signalée par un étudiant';
+$string['pluginname'] = 'Signalement d’absences';
+$string['report_link'] = 'Voir le rapport des absences signalées';
+$string['request_submitted'] = 'Votre absence signalée a été soumise. Note : il appartient à l’étudiant de prendre des dispositions avec chaque enseignant pour le travail manqué.';
+$string['short_term_health'] = 'Conditions de santé à court terme (maladie, blessure, chirurgie programmée)';
+$string['sisid'] = 'Numéro d’étudiant';
+$string['student_firstname'] = 'Prénom de l’étudiant';
+$string['student_instructions'] = '<p>Veuillez remplir le formulaire ci-dessous pour signaler une absence. Vous pouvez soumettre au maximum deux absences par trimestre. Assurez-vous que votre absence ne dépasse pas 7 jours. Toute raison autre que celles listées dans le menu déroulant n’est pas éligible pour une absence auto-déclarée. Notez que cette absence signalée sera soumise pour tous les cours auxquels vous êtes inscrit ce trimestre.</p>'
+    . '<p>Toute disposition pour le travail manqué doit être prise avec chacun de vos enseignants.</p>'
+    . '<p>Pour plus d’informations sur la politique de considération académique pour le travail manqué, veuillez consulter la <a href="20250227_Senate_approved_Acad Consid_for_Missed_Course_Work_Policy_Final.pdf" target="_blank">Politique de considération académique</a>.</p>';
+$string['student_lastname'] = 'Nom de famille de l’étudiant';
+$string['submit_request'] = 'Signaler une absence';
+$string['to_date'] = 'Date de fin';
+$string['type_of_circumstance'] = 'Type de circonstance';
+$string['unforeseen'] = 'Incidents imprévus ou inévitables hors du contrôle de l’étudiant';
+$string['view_faculty_report'] = 'Rapport d’absence par faculté';
 
-// Paramètres de la page de configuration
-$string['requests_per_term'] = 'Nombre de demandes par session';
-$string['requests_per_term_desc'] = 'Définissez le nombre maximum de demandes d\'absence qu\'un étudiant peut soumettre par session.';
-$string['teacher_message'] = 'Message enseignant';
-$string['teacher_message_desc'] = 'Message par défaut à envoyer aux enseignants lorsqu\'une demande d\'absence est soumise.';
-$string['student_message'] = 'Message étudiant';
-$string['student_message_desc'] = 'Message par défaut à envoyer aux étudiants lorsqu\'une demande d\'absence est soumise.';
-$string['use_period'] = 'Période d\'utilisation';
-$string['use_period_desc'] = 'Ce paramètre est uniquement à utiliser lors des tests du plugin. '
-    . 'Il permet de sélectionner la période pendant laquelle les demandes d\'absence peuvent être soumises. '
-    . 'Note : L\'année est toujours disponible comme Hiver, jamais Automne. Donc, si vous testez pour des cours annuels, '
-    . 'assurez-vous de sélectionner une date entre janvier et avril.';
+// Settings page strings
+$string['requests_per_term'] = 'Nombre de demandes par trimestre';
+$string['requests_per_term_desc'] = 'Définissez le nombre maximum d’absences qu’un étudiant peut signaler par trimestre.';
+$string['teacher_message'] = 'Message pour l’enseignant';
+$string['teacher_message_desc'] = 'Message par défaut envoyé aux enseignants lorsqu’une absence est signalée.';
+$string['student_message'] = 'Message pour l’étudiant';
+$string['student_message_desc'] = 'Message par défaut envoyé aux étudiants lorsqu’une absence est signalée.';
+$string['use_period'] = 'Utiliser la période';
+$string['use_period_desc'] = 'Ce paramètre doit être utilisé uniquement lors des tests du plugin. Il vous permet de sélectionner la période pendant laquelle les absences signalées peuvent être soumises. Note : L’année est toujours disponible en hiver, jamais en automne. Donc, si vous testez pour les cours annuels, assurez-vous de sélectionner une date entre janvier et avril.';
 $string['use_period_no'] = 'Non';
 $string['use_period_F'] = 'Automne';
 $string['use_period_W'] = 'Hiver';
@@ -59,49 +53,49 @@ $string['use_period_S'] = 'Été';
 $string['use_period_Y'] = 'Année';
 
 // Notifications
-$string['absence_request:view_faculty_report'] = 'Voir le rapport d\'absence par faculté';
-$string['absence_request:view_teacher_report'] = 'Voir le rapport d\'absence';
-$string['messageprovider:absence_notification'] = 'Notifications de demande d\'absence';
-$string['student_message_subject'] = 'Demande d\'absence soumise';
-$string['teacher_message_subject'] = 'Notification de demande d\'absence';
+$string['absence_request:view_faculty_report'] = 'Voir le rapport d’absence par faculté';
+$string['absence_request:view_teacher_report'] = 'Voir le rapport d’absence';
+$string['messageprovider:absence_notification'] = 'Notifications de demande d’absence';
+$string['student_message_subject'] = 'Absence signalée avec succès';
+$string['teacher_message_subject'] = 'Notification d’absence signalée';
 $string['student_message'] = 'Bonjour {$a->firstname}, '
-    .'<p>Votre demande d\'absence a été soumise avec succès.</p>'
+    . '<p>Votre absence signalée a été soumise avec succès.</p>'
     . '<p>'
-    . '<b>Circonstance:</b> {$a->circumstance}<br>'
-    . '<b>Date de début:</b> {$a->startdate}<br>'
-    . '<b>Date de fin:</b> {$a->enddate}<br>'
+    . '<b>Circconstance :</b> {$a->circumstance}<br>'
+    . '<b>Date de début :</b> {$a->startdate}<br>'
+    . '<b>Date de fin :</b> {$a->enddate}<br>'
     . '</p>'
-    . '<p>Toute adaptation pour le travail de cours manqué doit être organisée avec vos instructeurs individuels</p>'
-    . '<p>Merci!</p>';
-$string['teacher_message'] = 'Bonjour, <p>Vous avez reçu une absence auto-déclarée selon la politique de considération '
-    . 'académique pour le <a href="{$a->policylink}">travail de cours manqué</a>. Les détails sont ci-dessous.</p>'
+    . '<p>Toute disposition pour le travail manqué doit être prise avec chacun de vos enseignants.</p>'
+    . '<p>Merci !</p>';
+$string['teacher_message'] = 'Bonjour, <p>Vous avez reçu une absence auto-déclarée dans le cadre de la <a href="{$a->policylink}">Politique de considération académique pour le travail manqué</a>. Les détails sont ci-dessous.</p>'
     . '<p>'
-    . '<b>Étudiant:</b> {$a->studentname} ({$a->idnumber})<br>'
-    . '<b>Circonstance:</b> {$a->circumstance}<br>'
-    . '<b>Date de début:</b> {$a->startdate}<br>'
-    . '<b>Date de fin:</b> {$a->enddate}<br>'
+    . '<b>Étudiant :</b> {$a->studentname} ({$a->idnumber})<br>'
+    . '<b>Circconstance :</b> {$a->circumstance}<br>'
+    . '<b>Date de début :</b> {$a->startdate}<br>'
+    . '<b>Date de fin :</b> {$a->enddate}<br>'
     . '</p>'
-    . '<p>Vous pouvez également consulter toutes les demandes dans le <a href="{$a->url}">rapport des absences.</a></p>'
-    . '<p>Merci!</p>';
+    . '<p>Vous pouvez également consulter toutes les demandes dans le <a href="{$a->url}">rapport d’absence</a>.</p>'
+    . '<p>Merci !</p>';
 
-// Chaînes de l'API de confidentialité
-$string['privacy:metadata:local_absence_request'] = 'Informations sur les demandes d\'absence des étudiants';
-$string['privacy:metadata:local_absence_request:userid'] = 'L\'identifiant de l\'utilisateur qui a soumis la demande d\'absence';
-$string['privacy:metadata:local_absence_request:faculty'] = 'La faculté associée à la demande d\'absence';
-$string['privacy:metadata:local_absence_request:circumstance'] = 'La circonstance de la demande d\'absence';
-$string['privacy:metadata:local_absence_request:starttime'] = 'L\'heure de début de l\'absence';
-$string['privacy:metadata:local_absence_request:endtime'] = 'L\'heure de fin de l\'absence';
-$string['privacy:metadata:local_absence_request:acadyear'] = 'L\'année académique de la demande d\'absence';
-$string['privacy:metadata:local_absence_request:termperiod'] = 'La période de session de la demande d\'absence';
-$string['privacy:metadata:local_absence_request:timecreated'] = 'L\'heure à laquelle la demande d\'absence a été créée';
+// Privacy API strings
+$string['privacy:metadata:local_absence_request'] = 'Informations sur l’absence signalée par l’étudiant';
+$string['privacy:metadata:local_absence_request:userid'] = 'L’identifiant de l’utilisateur ayant signalé l’absence';
+$string['privacy:metadata:local_absence_request:faculty'] = 'La faculté associée à l’absence signalée';
+$string['privacy:metadata:local_absence_request:circumstance'] = 'La circonstance de l’absence signalée';
+$string['privacy:metadata:local_absence_request:starttime'] = 'L’heure de début de l’absence';
+$string['privacy:metadata:local_absence_request:endtime'] = 'L’heure de fin de l’absence';
+$string['privacy:metadata:local_absence_request:acadyear'] = 'L’année universitaire de l’absence signalée';
+$string['privacy:metadata:local_absence_request:termperiod'] = 'La période de trimestre de l’absence signalée';
+$string['privacy:metadata:local_absence_request:timecreated'] = 'L’heure de création de l’absence signalée';
 
-$string['privacy:metadata:local_absence_req_teacher'] = 'Informations sur les enseignants associés aux demandes d\'absence';
-$string['privacy:metadata:local_absence_req_teacher:userid'] = 'L\'identifiant de l\'enseignant associé à la demande d\'absence';
-$string['privacy:metadata:local_absence_req_teacher:absence_req_course_id'] = 'L\'identifiant du cours de la demande d\'absence';
-$string['privacy:metadata:local_absence_req_teacher:timecreated'] = 'L\'heure à laquelle l\'association de l\'enseignant a été créée';
+$string['privacy:metadata:local_absence_req_teacher'] = 'Informations sur les enseignants associés à l’absence signalée';
+$string['privacy:metadata:local_absence_req_teacher:userid'] = 'L’identifiant de l’enseignant associé à l’absence signalée';
+$string['privacy:metadata:local_absence_req_teacher:absence_req_course_id'] = 'L’identifiant du cours concerné par l’absence signalée';
+$string['privacy:metadata:local_absence_req_teacher:timecreated'] = 'L’heure de création de l’association enseignant';
 
-$string['privacy:path:absencerequests'] = 'Demandes d\'absence';
+$string['privacy:path:absencerequests'] = 'Absences signalées';
 $string['privacy:path:teacherrequests'] = 'Notifications aux enseignants';
 
 $string['teacher_firstname'] = 'Prénom de l’enseignant';
 $string['teacher_lastname'] = 'Nom de famille de l’enseignant';
+
